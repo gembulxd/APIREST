6eb484cab2e6cdbe64d0fc1f2bc73d24bdfcfeda
gembulxd <132267574+gembulxd@users.noreply.github.com>
2023-06-04 15:47:05 +0700

    Update settings.js
    Splsodorppf

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'AnjasWibowo'// yourname
 global.MONGO_DB_URI = "mongodb+srv://banbowo56:nMjf4US3ipTAW05P@dedekanjas.jkeloko.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "annovstock@gmail.com" //email
-global.email_password = "hgdevjzqymcdwyre" //application password email
+global.your_email = "ibukkujsrwatii72@gmail.com" //email
+global.email_password = "kacxvtwwoacxgruq" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
